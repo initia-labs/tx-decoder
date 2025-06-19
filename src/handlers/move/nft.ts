7b7f2b37929e751e80d3abaa6929fd7c2124f069
evilpeach <kanisorn.peach@gmail.com>
2025-06-19 13:11:09 +0700

    fix: format

--- src/handlers/move/nft.ts
+++ src/handlers/move/nft.ts
@@ -1,7 +1,7 @@
-import { MessageDecoder } from "@/interfaces"
-import { Event, Message, MintNFTEvent, zMintNFTEvent } from "@/schema"
+import { MessageDecoder } from "@/interfaces";
+import { Event, Message, MintNFTEvent, zMintNFTEvent } from "@/schema";
 
-import { isMoveMessage } from "./index"
+import { isMoveMessage } from "./index";
 
 const isSimpleMint = (message: Message) => {
   return (
@@ -9,32 +9,33 @@ const isSimpleMint = (message: Message) => {
     message.module_address === "0x1" &&
     message.module_name === "simple_nft" &&
     message.function_name === "mint"
-  )
-}
+  );
+};
 
 const isUsernameMint = (message: Message) => {
   return (
     isMoveMessage(message) &&
-    message.module_address === "0x72ed9b26ecdcd6a21d304df50f19abfdbe31d2c02f60c84627844620a45940ef" &&
+    message.module_address ===
+      "0x72ed9b26ecdcd6a21d304df50f19abfdbe31d2c02f60c84627844620a45940ef" &&
     message.module_name === "usernames" &&
     message.function_name === "register_domain"
-  )
-}
+  );
+};
 
 export const nftMintDecoder: MessageDecoder = {
   check: (message) => {
-    return isSimpleMint(message) || isUsernameMint(message)
+    return isSimpleMint(message) || isUsernameMint(message);
   },
-  decode: (message, log) => {
-    const mintEvent = findMintEvent(log.events)
+  decode: (_message, log) => {
+    const mintEvent = findMintEvent(log.events);
     if (!mintEvent) {
-      throw new Error("NFT Mint event not found")
+      throw new Error("NFT Mint event not found");
     }
 
     return {
       balanceChanges: {
         ft: {},
-        nft: {}
+        nft: {},
       },
       decodedMessage: {
         action: "nft_mint",
@@ -47,9 +48,9 @@ export const nftMintDecoder: MessageDecoder = {
         isIbc: false,
         isOp: false,
       },
-    }
+    };
   },
-}
+};
 
 // internal parser
 function findMintEvent(events: Event[]): MintNFTEvent | null {
@@ -60,20 +61,22 @@ function findMintEvent(events: Event[]): MintNFTEvent | null {
         (attr) =>
           attr.key === "type_tag" && attr.value === "0x1::collection::MintEvent"
       )
-  )
+  );
 
-  if (!mintEvent) return null
+  if (!mintEvent) return null;
 
-  const dataAttribute = mintEvent.attributes.find((attr) => attr.key === "data")
+  const dataAttribute = mintEvent.attributes.find(
+    (attr) => attr.key === "data"
+  );
 
   if (!dataAttribute) {
-    return null
+    return null;
   }
 
-  const parsed = zMintNFTEvent.safeParse(JSON.parse(dataAttribute.value))
+  const parsed = zMintNFTEvent.safeParse(JSON.parse(dataAttribute.value));
   if (!parsed.success) {
-    return null
+    return null;
   }
 
-  return parsed.data
+  return parsed.data;
 }
