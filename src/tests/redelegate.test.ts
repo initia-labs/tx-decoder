2f5e86da26c7b539d718f17f2b0e450ed7475588
evilpeach <kanisorn.peach@gmail.com>
2025-06-27 10:02:48 +0700

    feat: update redelegate, withdraw reward

--- src/tests/redelegate.test.ts
+++ src/tests/redelegate.test.ts
@@ -1,12 +1,17 @@
-import { mockMsgRedelegate } from "@/tests/fixtures/redelegate.fixture";
+import axios from "axios";
 
-import { initialize } from "./helpers";
+import { mockApiResponsesRedelegate, mockMsgRedelegate } from "@/tests/fixtures/redelegate.fixture";
+
+import { createMockApiHandler, initialize } from "./helpers";
 
 jest.mock("axios");
 const decoder = initialize();
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("Redelegate Message", () => {
   it("should decode a redelegate message correctly", async () => {
+    mockedAxios.get.mockImplementation(createMockApiHandler(mockApiResponsesRedelegate));
+
     const decoded = await decoder.decodeTransaction(mockMsgRedelegate);
 
     expect(decoded.messages).toHaveLength(1);
@@ -20,25 +25,35 @@ describe("Redelegate Message", () => {
           },
         ],
         delegatorAddress: "init1kw2unuhgfa6mz6r0ehrzlr9k9ftjk7pql8u5fm",
-        validatorDstAddress:
-          "initvaloper19uzc087w778p0l333w52ju0dgsajcj6ydep4rm",
-        validatorSrcAddress:
-          "initvaloper1cmlx2pqfgt2kpshe2fmc40epzvg699eqv3ax66",
+        validatorDstAddress: "initvaloper19uzc087w778p0l333w52ju0dgsajcj6ydep4rm",
+        validatorSrcAddress: "initvaloper1cmlx2pqfgt2kpshe2fmc40epzvg699eqv3ax66",
       },
       isIbc: false,
       isOp: false,
     });
 
-    // expect(decoded.balanceChanges).toEqual({
-    //   ft: {
-    //     initvaloper1cmlx2pqfgt2kpshe2fmc40epzvg699eqv3ax66: {
-    //       uinit: "-49340",
-    //     },
-    //     initvaloper19uzc087w778p0l333w52ju0dgsajcj6ydep4rm: {
-    //       uinit: "49340",
-    //     },
-    //   },
-    //   object: {},
-    // });
+    expect(decoded.messages[0].balanceChanges).toEqual({
+      ft: {
+        init1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8ffy0za: {
+          uinit: "-2",
+        },
+        init1kw2unuhgfa6mz6r0ehrzlr9k9ftjk7pql8u5fm: {
+          uinit: "2",
+        },
+      },
+      object: {},
+    });
+
+    expect(decoded.totalBalanceChanges).toEqual({
+      ft: {
+        init1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8ffy0za: {
+          uinit: "-2",
+        },
+        init1kw2unuhgfa6mz6r0ehrzlr9k9ftjk7pql8u5fm: {
+          uinit: "2",
+        },
+      },
+      object: {},
+    });
   });
 });
--- src/tests/withdraw-delegator-reward.test.ts
+++ src/tests/withdraw-delegator-reward.test.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 import {
   mockApiResponsesWithdrawDelegatorReward,
-  mockApiResponsesWithdrawDelegatorRewardWithMultipleCoins,
   mockMsgWithdrawDelegatorReward,
-  mockMsgWithdrawDelegatorRewardWithMultipleCoins,
 } from "./fixtures/withdraw-delegator-reward.fixture";
 import { createMockApiHandler, initialize } from "./helpers";
 
@@ -15,14 +13,10 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("Withdraw Delegator Reward Message", () => {
   it("should decode a withdraw delegator reward message correctly", async () => {
-    const mockApiHandler = createMockApiHandler(
-      mockApiResponsesWithdrawDelegatorReward
-    );
+    const mockApiHandler = createMockApiHandler(mockApiResponsesWithdrawDelegatorReward);
     mockedAxios.get.mockImplementation(mockApiHandler);
 
-    const decoded = await decoder.decodeTransaction(
-      mockMsgWithdrawDelegatorReward
-    );
+    const decoded = await decoder.decodeTransaction(mockMsgWithdrawDelegatorReward);
 
     const { messages, totalBalanceChanges } = decoded;
     const { balanceChanges, decodedMessage } = messages[0];
@@ -56,52 +50,6 @@ describe("Withdraw Delegator Reward Message", () => {
         init13thkj7pxgr3l6hzymklfeprh0fka8n6mzckr78: {
           uinit: "33490255",
         },
-      },
-      object: {},
-    });
-  });
-
-  it("should decode a withdraw delegator reward message with multiple coins correctly", async () => {
-    const mockApiHandler = createMockApiHandler(
-      mockApiResponsesWithdrawDelegatorRewardWithMultipleCoins
-    );
-    mockedAxios.get.mockImplementation(mockApiHandler);
-
-    const decoded = await decoder.decodeTransaction(
-      mockMsgWithdrawDelegatorRewardWithMultipleCoins
-    );
-
-    const { messages, totalBalanceChanges } = decoded;
-    const { balanceChanges, decodedMessage } = messages[0];
-
-    expect(messages).toHaveLength(1);
-    expect(decodedMessage).toEqual({
-      action: "withdraw_delegator_reward",
-      data: {
-        coins: [
-          { amount: "33490255", denom: "uinit" },
-          { amount: "99999", denom: "ustake" },
-          { amount: "1000000", denom: "utoken" },
-        ],
-        from: "init13thkj7pxgr3l6hzymklfeprh0fka8n6mzckr78",
-        validatorAddress: "initvaloper1gs7kwd5jm8ghnvx4z973aqcenfcej6ykpuydc4",
-      },
-      isIbc: false,
-      isOp: false,
-    });
-
-    expect(balanceChanges).toEqual({
-      ft: {
-        init1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8ffy0za: { uinit: "-33490255" },
-        init13thkj7pxgr3l6hzymklfeprh0fka8n6mzckr78: { uinit: "33490255" },
-      },
-      object: {},
-    });
-
-    expect(totalBalanceChanges).toEqual({
-      ft: {
-        init1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8ffy0za: { uinit: "-33490255" },
-        init13thkj7pxgr3l6hzymklfeprh0fka8n6mzckr78: { uinit: "33490255" },
       },
       object: {},
     });
