dc41db54bbc660bc33855d22165adec06f45e6f8
alecande11 <54709706+alecande11@users.noreply.github.com>
2025-07-30 12:41:52 +0200

    Add tests for balances

--- src/tests/move/deposit-liquidity.test.ts
+++ src/tests/move/deposit-liquidity.test.ts
@@ -47,6 +47,42 @@ describe("Deposit Liquidity Message", () => {
       isIbc: false,
       isOp: false,
     });
+
+    expect(decoded.messages[0].balanceChanges).toEqual({
+      ft: {
+        init12santguultdd8k3uyvjfc36y2hg4alf0jnuyj3ejym0w3g7848ssynvde3: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "1000000",
+          uinit: "8415380",
+        },
+        init15j9nswsatns09fnru6ww9jjljg07r87kr56mdc: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "-1000000",
+          uinit: "-8415380",
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "8372391",
+        },
+      },
+      object: {},
+    });
+
+    expect(decoded.totalBalanceChanges).toEqual({
+      ft: {
+        init12santguultdd8k3uyvjfc36y2hg4alf0jnuyj3ejym0w3g7848ssynvde3: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "1000000",
+          uinit: "8415380",
+        },
+        init15j9nswsatns09fnru6ww9jjljg07r87kr56mdc: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "-1000000",
+          uinit: "-8415380",
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "8372391",
+        },
+      },
+      object: {},
+    });
   });
 
   it("should decode a deposit stake liquidity move message correctly", async () => {
@@ -84,6 +120,50 @@ describe("Deposit Liquidity Message", () => {
       },
       isIbc: false,
       isOp: false,
+    });
+
+    expect(decoded.messages[0].balanceChanges).toEqual({
+      ft: {
+        init12santguultdd8k3uyvjfc36y2hg4alf0jnuyj3ejym0w3g7848ssynvde3: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "100000",
+          uinit: "841538",
+        },
+        init15j9nswsatns09fnru6ww9jjljg07r87kr56mdc: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "-100000",
+          uinit: "-841538",
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "0",
+        },
+        init1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3mdfuj4: {
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "837235",
+        },
+      },
+      object: {},
+    });
+
+    expect(decoded.totalBalanceChanges).toEqual({
+      ft: {
+        init12santguultdd8k3uyvjfc36y2hg4alf0jnuyj3ejym0w3g7848ssynvde3: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "100000",
+          uinit: "841538",
+        },
+        init15j9nswsatns09fnru6ww9jjljg07r87kr56mdc: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "-100000",
+          uinit: "-841538",
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "0",
+        },
+        init1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3mdfuj4: {
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "837235",
+        },
+      },
+      object: {},
     });
   });
 
@@ -123,6 +203,64 @@ describe("Deposit Liquidity Message", () => {
       },
       isIbc: false,
       isOp: false,
+    });
+
+    expect(decoded.messages[0].balanceChanges).toEqual({
+      ft: {
+        init12santguultdd8k3uyvjfc36y2hg4alf0jnuyj3ejym0w3g7848ssynvde3: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "1000000",
+          uinit: "8449282",
+        },
+        init15j9nswsatns09fnru6ww9jjljg07r87kr56mdc: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "-1000000",
+          uinit: "-8449271",
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "0",
+        },
+        init1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3mdfuj4: {
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "8399326",
+        },
+        init1fzny967x2nsn4mcp6t7azlrx4mxn97zdelgzlfnw39scs6vrwhcqglyesr: {
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "0",
+        },
+        init1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8ffy0za: {
+          uinit: "-11",
+        },
+      },
+      object: {},
+    });
+
+    expect(decoded.totalBalanceChanges).toEqual({
+      ft: {
+        init12santguultdd8k3uyvjfc36y2hg4alf0jnuyj3ejym0w3g7848ssynvde3: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "1000000",
+          uinit: "8449282",
+        },
+        init15j9nswsatns09fnru6ww9jjljg07r87kr56mdc: {
+          "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4":
+            "-1000000",
+          uinit: "-8449271",
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "0",
+        },
+        init1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3mdfuj4: {
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "8399326",
+        },
+        init1fzny967x2nsn4mcp6t7azlrx4mxn97zdelgzlfnw39scs6vrwhcqglyesr: {
+          "move/543b35a39cfadad3da3c23249c474455d15efd2f94f849473226dee8a3c7a9e1":
+            "0",
+        },
+        init1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8ffy0za: {
+          uinit: "-11",
+        },
+      },
+      object: {},
     });
   });
 });
