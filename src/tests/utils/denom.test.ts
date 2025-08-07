9b27c9a752e3acfe880ae6e7cc84e89b4acf99e1
evilpeach <kanisorn.peach@gmail.com>
2025-08-07 16:00:57 +0700

    feat: add mock API response for chain data in vip-lock-stake fixture and update import path in denom tests

--- src/tests/utils/denom.test.ts
+++ src/tests/utils/denom.test.ts
@@ -1,4 +1,4 @@
-import { getIbcDenom, getOpDenom } from "@/utils/denom";
+import { getIbcDenom, getOpDenom } from "@/utils";
 
 describe("getIbcDenom", () => {
   it("should return correct ibc denom for channel-27 and l2/2b2d36f666e98b9eecf70d6ec24b882b79f2c8e2af73f54f97b8b670dbb87605", () => {
