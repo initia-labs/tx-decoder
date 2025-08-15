cdd430296ecc5e69f72e72807388a91f6bb57dc1
Kanisorn Peach <peach@initia.xyz>
2025-08-15 13:57:42 +0700

    fix: update vip gauge vote decoder to use max voting power and enhance test coverage (#57)
    * Changed the calculation of the voting amount in the vipGaugeVoteDecoder to use max_voting_power instead of voting_power.
    * Added mock data for testing with max voting power in the vip-gauge-vote fixture.
    * Enhanced tests to validate decoding of VIP gauge vote messages with max voting power.

--- src/tests/fixtures/vip-gauge-vote.fixture.ts
+++ src/tests/fixtures/vip-gauge-vote.fixture.ts
@@ -8,6 +8,344 @@ export const mockMsgVipGaugeVote = {
         {
           index: true,
           key: "sender",
+          value: "0x1,0x6baa5dcfd050e9b85a4ddf214baee77884773ba4"
+        },
+        {
+          index: true,
+          key: "module_addr",
+          value: "0x1"
+        },
+        {
+          index: true,
+          key: "module_name",
+          value: "coin"
+        },
+        {
+          index: true,
+          key: "function_name",
+          value: "sudo_transfer"
+        }
+      ],
+      type: "execute"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "type_tag",
+          value: "0x1::fungible_asset::WithdrawEvent"
+        },
+        {
+          index: true,
+          key: "data",
+          value:
+            '{"store_addr":"0x37736f7c96f6ba1dde1ec152e14c705a333b172a658cdd0d22b434122d712e16","metadata_addr":"0xe0e9394b24e53775d6af87934ac02d73536ad58b7894f6ccff3f5e7c0d548e55","amount":"8696"}'
+        }
+      ],
+      type: "move"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "type_tag",
+          value: "0x1::fungible_asset::WithdrawOwnerEvent"
+        },
+        {
+          index: true,
+          key: "data",
+          value: '{"owner":"0x6baa5dcfd050e9b85a4ddf214baee77884773ba4"}'
+        }
+      ],
+      type: "move"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "type_tag",
+          value: "0x1::fungible_asset::DepositEvent"
+        },
+        {
+          index: true,
+          key: "data",
+          value:
+            '{"store_addr":"0x5c8edb2aff315409e899a3cbabeaa8741eb8044448d2c28a37a8d0d6e5d96528","metadata_addr":"0xe0e9394b24e53775d6af87934ac02d73536ad58b7894f6ccff3f5e7c0d548e55","amount":"8696"}'
+        }
+      ],
+      type: "move"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "type_tag",
+          value: "0x1::fungible_asset::DepositOwnerEvent"
+        },
+        {
+          index: true,
+          key: "data",
+          value: '{"owner":"0xf1829676db577682e944fc3493d451b67ff3e29f"}'
+        }
+      ],
+      type: "move"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "spender",
+          value: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06"
+        },
+        {
+          index: true,
+          key: "amount",
+          value:
+            "8696ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4"
+        }
+      ],
+      type: "coin_spent"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "receiver",
+          value: "init17xpfvakm2amg962yls6f84z3kell8c5l70rnql"
+        },
+        {
+          index: true,
+          key: "amount",
+          value:
+            "8696ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4"
+        }
+      ],
+      type: "coin_received"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "recipient",
+          value: "init17xpfvakm2amg962yls6f84z3kell8c5l70rnql"
+        },
+        {
+          index: true,
+          key: "sender",
+          value: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06"
+        },
+        {
+          index: true,
+          key: "amount",
+          value:
+            "8696ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4"
+        }
+      ],
+      type: "transfer"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "sender",
+          value: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06"
+        }
+      ],
+      type: "message"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "fee",
+          value:
+            "8696ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4"
+        },
+        {
+          index: true,
+          key: "fee_payer",
+          value: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06"
+        }
+      ],
+      type: "tx"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "acc_seq",
+          value: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06/428"
+        }
+      ],
+      type: "tx"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "signature",
+          value:
+            "gN21v2rpM1dAvGrxX2Bo6fuOn/mFSWyskX3I+2/Z5WJZRmBmeSxG/pDxz8RrEctbAKN52fUa2y5YQ6ogNvIA2w=="
+        }
+      ],
+      type: "tx"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "action",
+          value: "/initia.move.v1.MsgExecute"
+        },
+        {
+          index: true,
+          key: "sender",
+          value: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06"
+        },
+        {
+          index: true,
+          key: "module",
+          value: "move"
+        },
+        {
+          index: true,
+          key: "msg_index",
+          value: "0"
+        }
+      ],
+      type: "message"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "sender",
+          value: "0x6baa5dcfd050e9b85a4ddf214baee77884773ba4"
+        },
+        {
+          index: true,
+          key: "module_addr",
+          value:
+            "0x3a886b32a802582f2e446e74d4a24d1d7ed01adf46d2a8f65c5723887e708789"
+        },
+        {
+          index: true,
+          key: "module_name",
+          value: "weight_vote"
+        },
+        {
+          index: true,
+          key: "function_name",
+          value: "vote"
+        },
+        {
+          index: true,
+          key: "msg_index",
+          value: "0"
+        }
+      ],
+      type: "execute"
+    },
+    {
+      attributes: [
+        {
+          index: true,
+          key: "type_tag",
+          value:
+            "0x3a886b32a802582f2e446e74d4a24d1d7ed01adf46d2a8f65c5723887e708789::weight_vote::VoteEvent"
+        },
+        {
+          index: true,
+          key: "data",
+          value:
+            '{"account":"0x6baa5dcfd050e9b85a4ddf214baee77884773ba4","cycle":"8","max_voting_power":"8310741","voting_power":"249322","weights":[{"bridge_id":"32","weight":"0.02"},{"bridge_id":"31","weight":"0.01"}]}'
+        },
+        {
+          index: true,
+          key: "msg_index",
+          value: "0"
+        }
+      ],
+      type: "move"
+    }
+  ],
+  gas_used: "945801",
+  gas_wanted: "1328147",
+  height: "5945467",
+  info: "",
+  logs: [],
+  raw_log: "",
+  timestamp: "2025-08-08T09:28:31Z",
+  tx: {
+    "@type": "/cosmos.tx.v1beta1.Tx",
+    auth_info: {
+      fee: {
+        amount: [
+          {
+            amount: "8696",
+            denom:
+              "ibc/6490A7EAB61059BFC1CDDEB05917DD70BDF3A611654162A1A47DB930D40D8AF4"
+          }
+        ],
+        gas_limit: "1328147",
+        granter: "",
+        payer: ""
+      },
+      signer_infos: [
+        {
+          mode_info: {
+            single: {
+              mode: "SIGN_MODE_EIP_191"
+            }
+          },
+          public_key: {
+            "@type": "/initia.crypto.v1beta1.ethsecp256k1.PubKey",
+            key: "A4shR8ybPUPxzBXqi4W6BH+MtTkuonZcw3xOFkT+7tE1"
+          },
+          sequence: "428"
+        }
+      ],
+      tip: null
+    },
+    body: {
+      extension_options: [],
+      memo: "",
+      messages: [
+        {
+          "@type": "/initia.move.v1.MsgExecute",
+          args: [
+            "CAAAAAAAAAA=",
+            "Ah8AAAAAAAAAIAAAAAAAAAA=",
+            "AgcAAMFv8oYjBwAAgt/kDUc="
+          ],
+          function_name: "vote",
+          module_address:
+            "0x3a886b32a802582f2e446e74d4a24d1d7ed01adf46d2a8f65c5723887e708789",
+          module_name: "weight_vote",
+          sender: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06",
+          type_args: []
+        }
+      ],
+      non_critical_extension_options: [],
+      timeout_height: "0"
+    },
+    signatures: [
+      "gN21v2rpM1dAvGrxX2Bo6fuOn/mFSWyskX3I+2/Z5WJZRmBmeSxG/pDxz8RrEctbAKN52fUa2y5YQ6ogNvIA2w=="
+    ]
+  },
+  txhash: "B86C951BAFE9163F7FDCE2159B397FAB86664BA9AE583C7F4F23CABE2C0BF1FA"
+};
+
+export const mockMsgVipGaugeVoteWithMaxVotingPower = {
+  code: 0,
+  codespace: "",
+  data: "12240A222F696E697469612E6D6F76652E76312E4D736745786563757465526573706F6E7365",
+  events: [
+    {
+      attributes: [
+        {
+          index: true,
+          key: "sender",
           value: "0x1,0xb395c9f2e84f75b1686fcdc62f8cb62a572b7820"
         },
         {
@@ -344,64 +682,53 @@ export const mockApiResponsesForVipGaugeVote = {
           op_bridge_id: "32"
         }
       }
-    ],
-    "/initia/move/v1/accounts/0x66a8cb0bfb991610dcffb8a6543ac0887c7c5405b8f985ebed6d628fe50c4686/resources":
+    ]
+  }
+};
+
+export const mockApiResponsesForVipGaugeVoteWithMaxVotingPower = {
+  GET: {
+    "/chains.json": [
       {
-        pagination: {
-          next_key: null,
-          total: "0"
-        },
-        resources: [
-          {
-            address:
-              "0x66a8cb0bfb991610dcffb8a6543ac0887c7c5405b8f985ebed6d628fe50c4686",
-            move_resource:
-              '{"type":"0x1::object::ObjectCore","data":{"allow_ungated_transfer":false,"owner":"0xf1829676db577682e944fc3493d451b67ff3e29f","version":"1"}}',
-            raw_bytes:
-              "AAAAAAAAAAAAAAAA8YKWdttXdoLpRPw0k9RRtn/z4p8AAQAAAAAAAAA=",
-            struct_tag: "0x1::object::ObjectCore"
-          },
-          {
-            address:
-              "0x66a8cb0bfb991610dcffb8a6543ac0887c7c5405b8f985ebed6d628fe50c4686",
-            move_resource:
-              '{"type":"0x1::fungible_asset::FungibleStore","data":{"balance":"0","frozen":false,"metadata":{"inner":"0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9"}}}',
-            raw_bytes:
-              "jkczvavPfUr8PRTw3UbJv1L7D86eS5lsk54ZW4vIkdkAAAAAAAAAAAA=",
-            struct_tag: "0x1::fungible_asset::FungibleStore"
-          }
-        ]
+        chain_id: "echelon-1",
+        chain_name: "echelon",
+        metadata: {
+          ibc_channels: [],
+          op_bridge_id: "16"
+        }
       },
-    "/initia/move/v1/accounts/0xe2d99fcea59da8da2d4095cc11cb83e6ff80cf2e41522c678b8f152233d1b65e/resources":
       {
-        pagination: {
-          next_key: null,
-          total: "0"
-        },
-        resources: [
-          {
-            address:
-              "0x66a8cb0bfb991610dcffb8a6543ac0887c7c5405b8f985ebed6d628fe50c4686",
-            move_resource:
-              '{"type":"0x1::object::ObjectCore","data":{"allow_ungated_transfer":false,"owner":"0xf1829676db577682e944fc3493d451b67ff3e29f","version":"1"}}',
-            raw_bytes:
-              "AAAAAAAAAAAAAAAA8YKWdttXdoLpRPw0k9RRtn/z4p8AAQAAAAAAAAA=",
-            struct_tag: "0x1::object::ObjectCore"
-          },
-          {
-            address:
-              "0x66a8cb0bfb991610dcffb8a6543ac0887c7c5405b8f985ebed6d628fe50c4686",
-            move_resource:
-              '{"type":"0x1::fungible_asset::FungibleStore","data":{"balance":"0","frozen":false,"metadata":{"inner":"0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9"}}}',
-            raw_bytes:
-              "jkczvavPfUr8PRTw3UbJv1L7D86eS5lsk54ZW4vIkdkAAAAAAAAAAAA=",
-            struct_tag: "0x1::fungible_asset::FungibleStore"
-          }
-        ]
+        chain_id: "ingnetwork-1",
+        chain_name: "ingnetwork",
+        metadata: {
+          ibc_channels: [],
+          op_bridge_id: "27"
+        }
       },
-    "/initia/move/v1/denom?metadata=0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9":
       {
-        denom: "uinit"
+        chain_id: "rave-1",
+        chain_name: "rave",
+        metadata: {
+          ibc_channels: [],
+          op_bridge_id: "17"
+        }
+      },
+      {
+        chain_id: "intergaze-1",
+        chain_name: "intergaze",
+        metadata: {
+          ibc_channels: [],
+          op_bridge_id: "31"
+        }
+      },
+      {
+        chain_id: "inertia-2",
+        chain_name: "inertia",
+        metadata: {
+          ibc_channels: [],
+          op_bridge_id: "32"
+        }
       }
+    ]
   }
 };
