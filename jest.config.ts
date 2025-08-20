928f3ef187342f96f526973f823243663377f289
Kanisorn Peach <peach@initia.xyz>
2025-08-20 16:27:37 +0700

    chore: update Jest configuration to include coverage settings and add test coverage script (#60)

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,22 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "src/**/*.{ts,js}",
+    "!src/**/*.d.ts",
+    "!src/tests/**/*",
+    "!src/**/index.ts"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html", "json"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
