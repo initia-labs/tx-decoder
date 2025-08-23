8f53d8b35a22a92173055fd766694ae7a333d125
evilpeach <kanisorn.peach@gmail.com>
2025-08-23 08:53:30 +0700

    fix: update vip gauge vote decoder to use string representation and divide by 10^6

--- src/tests/move/vip-gauge-vote.test.ts
+++ src/tests/move/vip-gauge-vote.test.ts
@@ -29,20 +29,20 @@ describe("VIP Gauge Vote Message", () => {
       data: {
         epoch: "8",
         from: "init1dw49mn7s2r5mskjdmus5hth80zz8wwaywycq06",
-        maxVotingPower: "8310741",
+        maxVotingPower: "8.310741",
         votes: [
           {
-            amount: 166214.82,
+            amount: "0.16621482",
             rollup: "inertia-2",
-            weight: 0.02
+            weight: "0.02"
           },
           {
-            amount: 83107.41,
+            amount: "0.08310741",
             rollup: "intergaze-1",
-            weight: 0.01
+            weight: "0.01"
           }
         ],
-        votingPower: "249322"
+        votingPower: "0.249322"
       },
       isIbc: false,
       isOp: false
@@ -74,35 +74,35 @@ describe("VIP Gauge Vote Message", () => {
       data: {
         epoch: "8",
         from: "init1kw2unuhgfa6mz6r0ehrzlr9k9ftjk7pql8u5fm",
-        maxVotingPower: "903526",
+        maxVotingPower: "0.903526",
         votes: [
           {
-            amount: 90352.6,
+            amount: "0.0903526",
             rollup: "echelon-1",
-            weight: 0.1
+            weight: "0.1"
           },
           {
-            amount: 271057.8,
+            amount: "0.2710578",
             rollup: "ingnetwork-1",
-            weight: 0.3
+            weight: "0.3"
           },
           {
-            amount: 361410.4,
+            amount: "0.3614104",
             rollup: "rave-1",
-            weight: 0.4
+            weight: "0.4"
           },
           {
-            amount: 90352.6,
+            amount: "0.0903526",
             rollup: "intergaze-1",
-            weight: 0.1
+            weight: "0.1"
           },
           {
-            amount: 90352.6,
+            amount: "0.0903526",
             rollup: "inertia-2",
-            weight: 0.1
+            weight: "0.1"
           }
         ],
-        votingPower: "903526"
+        votingPower: "0.903526"
       },
       isIbc: false,
       isOp: false
