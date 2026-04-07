96f1b9077990b0c3b63715012a84ebe84cab5866
James0126 <tubleyes@gmail.com>
2026-04-07 13:56:51 +0900

    chore: remove tarball and add *.tgz to gitignore

--- src/constants/module-addresses.ts
+++ src/constants/module-addresses.ts
@@ -12,13 +12,3 @@ export const INITIA_VAULT_MODULE_ADDRESSES = [
   "0x3a886b32a802582f2e446e74d4a24d1d7ed01adf46d2a8f65c5723887e708789", // mainnet
   "0x81c3ea419d2fd3a27971021d9dd3cc708def05e5d6a09d39b2f1f9ba18312264" // testnet
 ] as const;
-
-export const CLAMM_MODULE_ADDRESSES = [
-  "0xd78a3b72c7ef0cfba7286bfb8c618aa4d6011dce05a832871cc9ab323c25f55e", // mainnet
-  "0x6b41bf295bc31cd9bef75a9a5a67e5a8d6749b34a7ab3105808251fa2697823d" // testnet
-] as const;
-
-export const CLAMM_FARMING_MODULE_ADDRESSES = [
-  "0xcb2999c70a9b8db7cb473255bb01f956f0726087f08b04ece50844a6d8167351", // mainnet
-  "0xf8ef0cb7c73607b7658524565015ce2aadc45ccf7164e5351959a4d7a1c37753" // testnet
-] as const;
